02dcfa31282b02e5ddc2499e3e4be4c848934d50
Zhiyang-Zhang-sjsu <43052894+Zhiyang-Zhang-sjsu@users.noreply.github.com>
2021-11-09 17:20:37 -0600

    Add files via upload

--- automatedScriptingFramework/excel_driver/execl_test_data_management.py
+++ automatedScriptingFramework/excel_driver/execl_test_data_management.py
@@ -1,8 +1,21 @@
 import openpyxl
+from openpyxl.styles import PatternFill, Font
 
 from web_keywords.web_UI_keywords import WebUIKeywords
 
+TESTING_RESULT_ROW = 16
+TESTING_RESULT_COL = 8
+
 class ExeclTestDataManagement:
+    def write_result(self, cell, row, col, status = False):
+        if status:
+            cell(row=row, column=col).value = "Passed"
+            cell(row=row, column=col).font = Font(bold=True, color = "00FF00")
+        else:
+            cell(row=row, column=col).value = "Failed"
+            cell(row=row, column=col).font = Font(bold=True, color = "FF0000")
+
+
     def run_test_data(self, file, log):
         driver = {}
         excel = openpyxl.load_workbook(file)
@@ -16,6 +29,7 @@ class ExeclTestDataManagement:
                         data["by"] = values[2]
                         data["value"] = values[3]
                         data["text"] = values[4]
+                        data['expected_result'] = values[6]
 
                         for key in list(data.keys()):
                             if data[key] is None:
@@ -28,6 +42,9 @@ class ExeclTestDataManagement:
                             res = getattr(keywords, values[1])(**data)
                             if values[1] == "get_driver":
                                 driver[name] = res
+                            elif "assert" in values[1]:
+                                self.write_result(sheet.cell, TESTING_RESULT_ROW, TESTING_RESULT_COL, res)
+                                excel.save(file)
         except Exception as e:
             log.exception(f"Exception: {e}")
         finally:
